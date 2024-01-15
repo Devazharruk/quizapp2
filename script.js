c5c20ae48ca019ad1207d8b4b63b29c9702d488a
azharali <azharruk66@gmail.com>
2024-01-15 07:06:57 -0800

    this is first commit

---
+++ script.js
@@ -0,0 +1,656 @@
+const pakistanMCQs = [
+    {
+        question: "Which year did Pakistan gain independence?",
+        options: [
+            "1945",
+            "1947",
+            "1952",
+            "1960",
+        ],
+        correct: 1,
+    },
+    {
+        question: "What is the capital city of Pakistan?",
+        options: [
+            "Karachi",
+            "Lahore",
+            "Islamabad",
+            "Rawalpindi",
+        ],
+        correct: 2,
+    },
+    // {
+    //     question: "Which river is the longest in Pakistan?",
+    //     options: [
+    //         "Indus River",
+    //         "Chenab River",
+    //         "Jhelum River",
+    //         "Ravi River",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "What is the national currency of Pakistan?",
+    //     options: [
+    //         "Peso",
+    //         "Rupee",
+    //         "Rupiah",
+    //         "Ringgit",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "Who is the founder of Pakistan?",
+    //     options: [
+    //         "Allama Iqbal",
+    //         "Liaquat Ali Khan",
+    //         "Muhammad Ali Jinnah",
+    //         "Ayub Khan",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which mountain range is located in the northern part of Pakistan?",
+    //     options: [
+    //         "Himalayas",
+    //         "Andes",
+    //         "Alps",
+    //         "Karakoram Range",
+    //     ],
+    //     correct: 3,
+    // },
+    // {
+    //     question: "What is the name of the highest peak in Pakistan?",
+    //     options: [
+    //         "K2",
+    //         "Nanga Parbat",
+    //         "Gasherbrum I",
+    //         "Rakaposhi",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani city is known as the 'City of Gardens'?",
+    //     options: [
+    //         "Lahore",
+    //         "Islamabad",
+    //         "Karachi",
+    //         "Faisalabad",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "In which year did Pakistan become a republic?",
+    //     options: [
+    //         "1956",
+    //         "1962",
+    //         "1971",
+    //         "1973",
+    //     ],
+    //     correct: 3,
+    // },
+    // {
+    //     question: "What is the official language of Pakistan?",
+    //     options: [
+    //         "Urdu",
+    //         "English",
+    //         "Punjabi",
+    //         "Sindhi",
+    //     ],
+    //     correct: 0,
+    // },
+
+    // {
+    //     question: "Which Pakistani scientist is known as the 'Father of Pakistan's Nuclear Program'?",
+    //     options: [
+    //         "Dr. Abdul Sattar Edhi",
+    //         "Dr. Abdul Qadeer Khan",
+    //         "Dr. Abdus Salam",
+    //         "Dr. Pervez Hoodbhoy",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the traditional dress worn by men in Pakistan?",
+    //     options: [
+    //         "Kurta",
+    //         "Sherwani",
+    //         "Shalwar Kameez",
+    //         "Dhoti",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which Pakistani cricketer has the nickname 'Rawalpindi Express'?",
+    //     options: [
+    //         "Shoaib Akhtar",
+    //         "Wasim Akram",
+    //         "Waqar Younis",
+    //         "Imran Khan",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "What is the largest province in Pakistan by area?",
+    //     options: [
+    //         "Sindh",
+    //         "Balochistan",
+    //         "Punjab",
+    //         "Khyber Pakhtunkhwa",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "Which Pakistani film won the Academy Award for Best Short Documentary in 2012?",
+    //     options: [
+    //         "Bol",
+    //         "Khuda Kay Liye",
+    //         "A Girl in the River: The Price of Forgiveness",
+    //         "Load Wedding",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which Pakistani musician is known for his contributions to Qawwali music?",
+    //     options: [
+    //         "Abida Parveen",
+    //         "Nusrat Fateh Ali Khan",
+    //         "Atif Aslam",
+    //         "Ali Zafar",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the historic fort located in Lahore, Pakistan?",
+    //     options: [
+    //         "Lal Qila",
+    //         "Shalimar Fort",
+    //         "Rohtas Fort",
+    //         "Lahore Fort",
+    //     ],
+    //     correct: 3,
+    // },
+    // {
+    //     question: "Which Pakistani player is known as the 'Sultan of Swing'?",
+    //     options: [
+    //         "Waqar Younis",
+    //         "Shoaib Akhtar",
+    //         "Wasim Akram",
+    //         "Imran Khan",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "What is the name of the traditional Pakistani bread?",
+    //     options: [
+    //         "Paratha",
+    //         "Roti",
+    //         "Naan",
+    //         "Chapati",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "Which Pakistani city is famous for its bazaars and is often referred to as the 'City of Bazaars'?",
+    //     options: [
+    //         "Islamabad",
+    //         "Lahore",
+    //         "Karachi",
+    //         "Peshawar",
+    //     ],
+    //     correct: 3,
+    // },
+
+    // {
+    //     question: "Which river is known as the 'River of Sorrow'?",
+    //     options: [
+    //         "Indus River",
+    //         "Jhelum River",
+    //         "Ravi River",
+    //         "Chenab River",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "What is the name of the traditional Pakistani headgear worn by men?",
+    //     options: [
+    //         "Topi",
+    //         "Pagri",
+    //         "Turban",
+    //         "Cap",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani singer is often referred to as the 'Queen of Melody'?",
+    //     options: [
+    //         "Noor Jehan",
+    //         "Abida Parveen",
+    //         "Nazia Hassan",
+    //         "Mehdi Hassan",
+    //     ],
+    //     correct: 3,
+    // },
+    // {
+    //     question: "Which mountain pass connects Pakistan with China?",
+    //     options: [
+    //         "Khyber Pass",
+    //         "Khunjerab Pass",
+    //         "Lowari Pass",
+    //         "Bolan Pass",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the famous desert located in the southeastern part of Pakistan?",
+    //     options: [
+    //         "Thar Desert",
+    //         "Cholistan Desert",
+    //         "Kharan Desert",
+    //         "Nushki Desert",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani city is known for its historic Mohenjo-daro archaeological site?",
+    //     options: [
+    //         "Multan",
+    //         "Haripur",
+    //         "Sukkur",
+    //         "Larkana",
+    //     ],
+    //     correct: 3,
+    // },
+    // {
+    //     question: "In which year did Pakistan win the ICC Cricket World Cup for the first time?",
+    //     options: [
+    //         "1992",
+    //         "2007",
+    //         "1987",
+    //         "1975",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani poet is often referred to as 'Shaair-e-Mashriq' (Poet of the East)?",
+    //     options: [
+    //         "Faiz Ahmed Faiz",
+    //         "Allama Iqbal",
+    //         "Ahmed Faraz",
+    //         "Parveen Shakir",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the largest salt mine in Pakistan?",
+    //     options: [
+    //         "Khewra Salt Mine",
+    //         "Warcha Salt Mine",
+    //         "Kalabagh Salt Mine",
+    //         "Gor Khuttree Salt Mine",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani actress won an Academy Award for Best Supporting Actress for her role in the film 'A Mighty Heart'?",
+    //     options: [
+    //         "Mahira Khan",
+    //         "Mehwish Hayat",
+    //         "Sajal Aly",
+    //         "Sharmeen Obaid-Chinoy",
+    //     ],
+    //     correct: 3,
+    // },
+
+    // {
+    //     question: "Which Pakistani city is known as the 'City of Lights'?",
+    //     options: [
+    //         "Karachi",
+    //         "Lahore",
+    //         "Islamabad",
+    //         "Faisalabad",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Who is the current Prime Minister of Pakistan (as of 2024)?",
+    //     options: [
+    //         "Imran Khan",
+    //         "Nawaz Sharif",
+    //         "Asif Ali Zardari",
+    //         "Bilawal Bhutto Zardari",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani scientist was awarded the Nobel Prize in Physics in 1979?",
+    //     options: [
+    //         "Abdul Sattar Edhi",
+    //         "Abdus Salam",
+    //         "Pervez Hoodbhoy",
+    //         "Atiyah Abd al-Rahman",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the famous mountain range that separates Pakistan from Afghanistan?",
+    //     options: [
+    //         "Hindu Kush",
+    //         "Pamir Mountains",
+    //         "Tien Shan",
+    //         "Zagros Mountains",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani cricket team captain led the country to its first-ever ICC T20 World Cup victory in 2009?",
+    //     options: [
+    //         "Shoaib Malik",
+    //         "Misbah-ul-Haq",
+    //         "Shahid Afridi",
+    //         "Younis Khan",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "What is the name of the famous lake situated in the Northern Areas of Pakistan?",
+    //     options: [
+    //         "Haleji Lake",
+    //         "Manchar Lake",
+    //         "Saif-ul-Mulook Lake",
+    //         "Keenjhar Lake",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which Pakistani female education activist and Nobel laureate was shot by the Taliban in 2012?",
+    //     options: [
+    //         "Asma Jahangir",
+    //         "Malala Yousafzai",
+    //         "Benazir Bhutto",
+    //         "Hina Rabbani Khar",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the traditional dance form originating from the Sindh region of Pakistan?",
+    //     options: [
+    //         "Bhangra",
+    //         "Kathak",
+    //         "Attan",
+    //         "Bhutto Dance",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which Pakistani mountain is often referred to as the 'Killer Mountain' due to its high fatality rate for climbers?",
+    //     options: [
+    //         "Nanga Parbat",
+    //         "Gasherbrum I",
+    //         "Broad Peak",
+    //         "Tirich Mir",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "In which year did Pakistan win its first Olympic gold medal in field hockey?",
+    //     options: [
+    //         "1948",
+    //         "1956",
+    //         "1960",
+    //         "1984",
+    //     ],
+    //     correct: 0,
+    // },
+
+    // {
+    //     question: "What is the name of the famous mountain pass located in the Swat Valley of Pakistan?",
+    //     options: [
+    //         "Shandur Pass",
+    //         "Babu Sar Top",
+    //         "Lowari Pass",
+    //         "Malam Jabba Pass",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which Pakistani author won the Booker Prize for Fiction in 1993 for the novel 'Riots: A Love Story'?",
+    //     options: [
+    //         "Mohsin Hamid",
+    //         "Kamila Shamsie",
+    //         "Bapsi Sidhwa",
+    //         "Arundhati Roy",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "What is the name of the traditional boat used in the waterways of Punjab and Sindh provinces?",
+    //     options: [
+    //         "Shikara",
+    //         "Sampan",
+    //         "Dhow",
+    //         "Houbara",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "Which Pakistani cricket player is nicknamed 'Boom Boom'?",
+    //     options: [
+    //         "Shoaib Akhtar",
+    //         "Shahid Afridi",
+    //         "Wahab Riaz",
+    //         "Fakhar Zaman",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the traditional Pakistani sweet made from condensed milk and sugar, often garnished with pistachios or almonds?",
+    //     options: [
+    //         "Jalebi",
+    //         "Gulab Jamun",
+    //         "Barfi",
+    //         "Halwa",
+    //     ],
+    //     correct: 3,
+    // },
+    // {
+    //     question: "Which Pakistani artist is known for his famous truck art and has gained international recognition?",
+    //     options: [
+    //         "Sadequain",
+    //         "Abdul Sattar Edhi",
+    //         "Imran Qureshi",
+    //         "Haider Ali",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "Which ancient civilization existed in the region that is now Pakistan?",
+    //     options: [
+    //         "Babylonian Civilization",
+    //         "Harappan Civilization",
+    //         "Mayan Civilization",
+    //         "Roman Civilization",
+    //     ],
+    //     correct: 1,
+    // },
+    // {
+    //     question: "What is the name of the famous mountain range in Balochistan, known for its distinct geological formations?",
+    //     options: [
+    //         "Toba Kakar Range",
+    //         "Sulaiman Mountains",
+    //         "Kirthar Range",
+    //         "Zarghun Range",
+    //     ],
+    //     correct: 0,
+    // },
+    // {
+    //     question: "Which Pakistani film won the Best Documentary Feature at the 2019 South Asian Film Festival?",
+    //     options: [
+    //         "Cake",
+    //         "Zinda Bhaag",
+    //         "Turtle: The Incredible Journey",
+    //         "Moor",
+    //     ],
+    //     correct: 2,
+    // },
+    // {
+    //     question: "In which city is the Allama Iqbal International Airport located?",
+    //     options: [
+    //         "Lahore",
+    //         "Islamabad",
+    //         "Karachi",
+    //         "Multan",
+    //     ],
+    //     correct: 0,
+    // },
+];
+
+console.log(pakistanMCQs.length);
+const questionElem = document.querySelector(".question h2");
+const optionElems = document.querySelectorAll(".opt");
+const nextBtn = document.querySelector(".next-btn");
+const currentQueElem = document.querySelector(".currentQue");
+const totalQue = document.querySelector(".total");
+const scoreCard = document.querySelector(".score-card");
+const restartBtn = document.querySelector(".restart-btn");
+const timeLeft = document.querySelector(".time-left");
+const timerLine = document.querySelector(".inner-line");
+const totalQuestions = document.querySelector(".total-question");
+
+let index = 0;
+let score = 0;
+let totalTime = 15;
+let remainingTime = totalTime;
+let intervalId;
+
+
+
+// Function for checking options
+function checkOption(selectedIndex, e) {
+    clearInterval(intervalId);
+    if (selectedIndex === pakistanMCQs[index].correct) {
+        e.target.classList.add("correct");
+        score++;
+    } else {
+        e.target.classList.add("incorrect");
+        optionElems[pakistanMCQs[index].correct].classList.add("correct");
+    }
+    disableOptions();
+    showNextButton();
+
+}
+
+// Function to load the next question..
+function loadNextQuestion() {
+    if (index < pakistanMCQs.length - 1) {
+        index++;
+        enableOptions();
+        loadQuiz();
+    } else {
+        showScore();
+    }
+    hideNextButton()
+}
+// Function to disable the options
+function disableOptions() {
+    optionElems.forEach((option) => {
+        option.removeEventListener("click", optionClickHandler);
+    });
+}
+
+// Function to enable the options
+function enableOptions() {
+    optionElems.forEach((option) => {
+        option.addEventListener("click", optionClickHandler);
+    });
+}
+
+// Function to show the next button
+function showNextButton() {
+    nextBtn.style.visibility = "visible";
+}
+
+// Function to hide the next button
+function hideNextButton() {
+    nextBtn.style.visibility = "hidden";
+}
+
+// Function to reset the options
+function resetOptions() {
+    optionElems.forEach((option) => {
+        option.classList.remove("correct", "incorrect");
+    })
+}
+
+// Function to show the score
+function showScore() {
+    totalQuestions.textContent = pakistanMCQs.length;
+    scoreCard.style.display = "flex";
+    document.querySelector(".score").textContent = score;
+}
+
+// Function to load the quizzes;
+function loadQuiz() {
+    questionElem.textContent = `${index + 1}. ${pakistanMCQs[index].question}`;
+    totalQue.textContent = pakistanMCQs.length;
+    currentQueElem.textContent = index + 1;
+
+    optionElems.forEach((option, ind) => {
+        option.textContent = pakistanMCQs[index].options[ind];
+    });
+
+    resetOptions();
+    quizTime();
+}
+
+// Function for countdown for the options
+function quizTime() {
+    totalTime = 15;
+    remainingTime = totalTime;
+    timerLine.style.width = '100%';
+
+    intervalId = setInterval(() => {
+        const elapsedPercentage = ((totalTime - remainingTime) / totalTime) * 100;
+        const lineWidth = 100 - elapsedPercentage;
+        timerLine.style.width = lineWidth + '%';
+        if (remainingTime <= 0) {
+            clearInterval(intervalId);
+            optionElems[pakistanMCQs[index].correct].classList.add("correct");
+            setTimeout(() => {
+                loadNextQuestion();
+            }, 1000);
+        }
+        if (remainingTime < 10) {
+            remainingTime = "0" + remainingTime;
+        }
+        timeLeft.textContent = remainingTime;
+        remainingTime--;
+    }, 1000);
+}
+
+// Function for handle the options
+function optionClickHandler(e) {
+    checkOption(Array.from(optionElems).indexOf(e.target), e);
+    console.log("option clicked");
+}
+
+optionElems.forEach((option) => {
+    option.addEventListener("click", optionClickHandler);
+})
+
+// Click functionality for next button
+nextBtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    loadNextQuestion();
+});
+
+// Click functionality for restart button
+restartBtn.addEventListener("click", () => {
+    scoreCard.style.display = "none";
+    index = 0;
+    score = 0;
+    loadQuiz();
+    enableOptions();
+});
+
+loadQuiz();
